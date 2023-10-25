36ebfc93276ac53e1f8354bf1716c8c45aec6ecb
Asus <s248818@studenti.polito.it>
2023-10-25 18:08:34 +0200

    cane

---
+++ main.py
@@ -0,0 +1,19 @@
+import git
+
+# Imposta il percorso locale della repository
+repo = git.Repo("https://github.com/OpenVirtualizedNetworks2023/python_basics_set1-alessiomongelli.git")
+
+# Visualizza l'elenco dei file modificati
+diff = repo.index.diff(None)
+for item in diff:
+    print(item)
+
+# Aggiungi tutti i file modificati al commit
+repo.index.add(diff)
+
+# Esegui un commit
+repo.index.commit("Messaggio del commit")
+
+# Push delle modifiche sulla repository remota
+origin = repo.remote(name='origin')
+origin.push()
